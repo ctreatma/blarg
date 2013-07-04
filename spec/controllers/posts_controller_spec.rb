6d242c56b3a949245d399baa5a8f4b8a0ddbf74c
Charles Treatman <charles.treatman@gmail.com>
2013-07-04 16:01:55 -0400

    Add controller for listing posts or viewing a single post

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe PostsController do
+
+  describe "GET 'index'" do
+    it "should return a list posts" do
+      post = Post.create(title: 'Test', content: 'Test content')
+      get :index
+      assigns(:posts).should eq([post])
+    end
+  end
+
+  describe "GET 'show'" do
+    it "should return a single post" do
+      post = Post.create(title: 'Test', content: 'Test content')
+      get :show, id: post.id
+      assigns(:post).should eq(post)
+    end
+  end
+
+end
