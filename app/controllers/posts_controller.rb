55d58488a9e5284aa2f21808c3e18149ee2e993b
Charles Treatman <charles.treatman@gmail.com>
2013-08-09 13:40:27 -0400

    Add an action to show posts by URL slug instead of primary key, and target that action when generating links in the view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,9 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def show_by_slug
+    @post = Post.find_by slug: params[:slug]
+    render 'show'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Blarg::Application.routes.draw do
     resources :posts, except: [:index, :show]
   end
   resources :posts, only: [:index, :show]
+  get ':slug' => 'posts#show_by_slug'
   root to: 'posts#index'
 end
